f0be2b38420acffa8808e1fb767359c7313035ca
Carry All <sugoi_maestro@hotmail.com>
2013-06-12 14:17:25 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Krudee::Application.config.secret_token = '054f6c5eb78c9e61f63d214e84c82a2d0bebb66f30cdb3918da64f4832cf4266eccb6efe05d349e3ac752dcb796a29b93cee5f6e60b6583c5e14ddf60ad27f97'
